caf3307ed12f57c0cc3f2744957c18cb77d58004
Stranger <rooderuler@gmail.com>
2021-09-25 19:23:37 +0300

    пройдено: 8 Списки и ключи (Lists & keys)

--- src/08_lists_and_keys/Lesson.jsx
+++ src/08_lists_and_keys/Lesson.jsx
@@ -22,6 +22,26 @@ const Tab3 = () => (
   <h1>Text of tab3</h1>
 )
 
+const people = ['Jack', 'Max', 'Leo', 'Alex']
+
+const TABS_BTN = [
+  {
+    dataName: 1,
+    title: 'Tab1',
+    icon: '+'
+  },
+  {
+    dataName: 2,
+    title: 'Tab2',
+    icon: '-'
+  },
+  {
+    dataName: 3,
+    title: 'Tab3',
+    icon: '*'
+  },
+]
+
 
 class App extends Component {
   state = {
@@ -40,20 +60,28 @@ class App extends Component {
     const {activeTab} = this.state
     return (
       <Fragment>
-        <button data-name={1} onClick={this.handleTab}>Tab1</button>
-        <button data-name={2} onClick={this.handleTab}>Tab2</button>
-        <button data-name={3} onClick={this.handleTab}>Tab3</button>
-        {/*{activeTab === 1 && <Tab1/>}*/}
-        {/*{activeTab === 2 && <Tab2/>}*/}
-        {/*{activeTab === 3 && <Tab3/>}*/}
+        {TABS_BTN.map(({dataName, title, icon}) => (
+          <button
+            key={`${dataName}-${title}`}
+            data-name={dataName}
+            onClick={this.handleTab}>
+            {icon}{title}
+          </button>
+        ))}
         {activeTab === 1 ? <Tab1/> : activeTab === 2 ? <Tab2/> : <Tab3/>}
         <div>
           {`Active tab is: ${activeTab === 1 ? 'first' : activeTab === 2 ? 'second' : 'third'}`}
         </div>
+        <ul>
+          {people.map((person, index) => (
+            <li key={index}>{person}</li>
+          ))}
+        </ul>
       </Fragment>
     )
   }
 }
+
 
 
 // const App = () => {
--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import ReactDOM from 'react-dom';
 // import Lesson from './04_props/Lesson'
 // import {Lesson, Counter, Button} from './05_props/Lesson'
 // import App from './06_events/Lesson'
-import App from './07_conditional_rendering/Lesson'
+// import App from './07_conditional_rendering/Lesson'
+import App from './08_lists_and_keys/Lesson'
 
 
 // import './index.css';Lesson.jsx
